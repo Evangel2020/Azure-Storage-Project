cd5957cc9a2ec7ade8bf682a86d0bad3f5491da2
Osinachi Chukwujama <Osinachi.Chukwujama@gmail.com>
2023-12-15 14:56:23 +0000

    chore: add toaster

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UploadPage from "./pages/upload";
 import DownloadPage from "./pages/download";
-
+import { Toaster } from "react-hot-toast";
 
 function App() {
-
   return (
     <>
+      <Toaster />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<UploadPage />} />
@@ -15,7 +15,7 @@ function App() {
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/upload/index.jsx
+++ src/pages/upload/index.jsx
@@ -1,75 +1,177 @@
-import { Flex, Box, Image, Button, Text, Heading, Divider, AbsoluteCenter, Input } from '@chakra-ui/react';
-import { useState } from 'react';
-import { useNavigate } from "react-router-dom"
-
-
+import {
+  Flex,
+  Box,
+  Image,
+  Button,
+  Text,
+  Heading,
+  Divider,
+  AbsoluteCenter,
+  Input,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const UploadPage = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const [file, setFile] = useState("");
+  const [key, setKey] = useState("");
+  //  To create a local Storage
+  localStorage.setItem("Image", file);
+  localStorage.setItem("Key", key);
 
-    const [image, setImage] = useState("")
-    const [key, setKey] = useState("")
-    //  To create a local Storage
-    localStorage.setItem("Image", image)
-    localStorage.setItem("Key", key)
+  const handleUpload = async () => {
+    try {
+      const formData = new FormData();
 
+      formData.append(
+        "containerName",
+        "store-a4dfe9c0-9b54-11ee-847a-fd406089e1df"
+      );
+      formData.append("file", file);
 
+      const response = await axios.post(
+        "https://victorious-puce-pigeon.cyclic.app/api/files/upload",
+        formData
+      );
+      const data = response.data;
 
-    return (
-        <Box width="100%" padding={{ base: "0.75rem", md: "2rem" }} h="100vh" bg='white' alignItems="center" justifyContent="center">
-            <Image src="/images/Azurelogo.png" width={{ base: "50%", md: "20%" }} height={{ base: "10%", md: "10%" }} alt='picture' />
-            <Flex height={{ base: "50vh", md: "80vh" }} borderRadius="20px" w={{ base: "95%", md: '80%' }} margin="50px auto" bg='gray.100' justifyContent="center" alignItems="center">
-                <Box w={{ base: "90%", md: "90%" }} border="5px dashed" borderColor='#48cae4' py={{ base: "10px", md: "80px" }} >
-                    <Box maxW={{ base: "100%" }} display="flex" justifyContent="center" alignItems="center">
-                        <Text>
-                            <Heading color="#0077b6" fontSize={{ base: "md", md: "5xl" }}>
-                                Drag and drop files here
-                            </Heading>
-                            <Box position='relative' padding='10'>
-                                <Divider />
-                                <AbsoluteCenter bg='transparent' px='12'>
-                                    or
-                                </AbsoluteCenter>
-                            </Box>
-                        </Text>
-                    </Box>
+      console.log(data);
+      toast.success("File uploaded successfully");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  return (
+    <Box
+      width="100%"
+      padding={{ base: "0.75rem", md: "2rem" }}
+      h="100vh"
+      bg="white"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Image
+        src="/images/Azurelogo.png"
+        width={{ base: "50%", md: "20%" }}
+        height={{ base: "10%", md: "10%" }}
+        alt="picture"
+      />
+      <Flex
+        height={{ base: "50vh", md: "80vh" }}
+        borderRadius="20px"
+        w={{ base: "95%", md: "80%" }}
+        margin="50px auto"
+        bg="gray.100"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box
+          w={{ base: "90%", md: "90%" }}
+          border="5px dashed"
+          borderColor="#48cae4"
+          py={{ base: "10px", md: "80px" }}
+        >
+          <Box
+            maxW={{ base: "100%" }}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Text>
+              <Heading color="#0077b6" fontSize={{ base: "md", md: "5xl" }}>
+                Drag and drop files here
+              </Heading>
+              <Box position="relative" padding="10">
+                <Divider />
+                <AbsoluteCenter bg="transparent" px="12">
+                  or
+                </AbsoluteCenter>
+              </Box>
+            </Text>
+          </Box>
 
-                    <Box display="flex" justifyContent="center" alignItems="center" gap={5} flexDirection="column" padding='4' color='black' w='100%' borderRadius="md" h="fit-content" >
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            gap={5}
+            flexDirection="column"
+            padding="4"
+            color="black"
+            w="100%"
+            borderRadius="md"
+            h="fit-content"
+          >
+            <Box
+              w={{ base: "50%", md: "100%" }}
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <label
+                style={{
+                  backgroundColor: "#0077b6",
+                  fontSize: "22px",
+                  padding: "20px 40px",
+                  color: "#fff",
+                  borderRadius: "7px",
+                  cursor: "pointer",
+                }}
+              >
+                <Input
+                  type={"file"}
+                  hidden
+                  onChange={(e) => {
+                    const file = e.target.files[0].name;
+                    setFile(file);
+                  }}
+                />
+                Browse Files
+              </label>
+            </Box>
 
+            <Input
+              htmlSize={30}
+              width="auto"
+              onChange={(e) => {
+                setKey(e.target.value);
+              }}
+              style={{
+                outline: "none",
+                border: "1px solid gray",
+                padding: "5px 10px",
+              }}
+            />
+          </Box>
+          {/* <img src={upLoaded} /> */}
+          <Box
+            display="flex"
+            justifyContent="center"
+            flexDirection="column"
+            alignItems="center"
+            marginTop={{ md: "3rem" }}
+          >
+            <Text color="gray" fontSize="2xl">
+              Maximum file size is 100mb
+            </Text>
+            <Button
+              onClick={handleUpload}
+              bg="blue.200"
+              padding="30px"
+              marginTop="4rem"
+            >
+              UPLOAD
+            </Button>
+          </Box>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
 
-                        <Box w={{ base: "50%", md: "100%" }} display="flex" justifyContent="center" alignItems="center">
-                            <label style={{ backgroundColor: "#0077b6", fontSize: "22px", padding: "20px 40px", color: "#fff", borderRadius: "7px", cursor: "pointer" }}>
-                                <Input
-                                    type={"file"}
-                                    hidden
-                                    onChange={(e) => {
-                                        const file = e.target.files[0].name;
-                                        setImage(file)
-                                    }}
-                                />
-                                Browse Files
-                            </label>
-
-
-                        </Box>
-
-                        <Input htmlSize={30} width='auto' onChange={(e) => { setKey(e.target.value) }} style={{ outline: "none", border: "1px solid gray", padding: "5px 10px" }} />
-
-                    </Box>
-                    {/* <img src={upLoaded} /> */}
-                    <Box display="flex" justifyContent="center" flexDirection="column" alignItems="center" marginTop={{ md: "3rem" }}>
-                        <Text color="gray" fontSize="2xl">
-                            Maximum file size is 100mb
-                        </Text>
-                        <Button onClick={() => navigate("/download")} bg="blue.200" padding='30px' marginTop="4rem">UPLOAD</Button>
-                    </Box>
-                </Box>
-            </Flex >
-        </Box >
-    )
-
-}
-
-export default UploadPage
+export default UploadPage;
